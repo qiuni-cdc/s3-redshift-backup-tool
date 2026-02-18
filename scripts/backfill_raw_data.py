e1680aae406990c5949125768c52e50810509696
JTung777 <jasleen.tung@uniuni.com>
2026-02-18 13:45:55 -0800

    Update backfill_raw_data.py

--- scripts/backfill_raw_data.py
+++ scripts/backfill_raw_data.py
@@ -50,7 +50,8 @@ class RawDataBackfiller:
             'id_column': 'order_id',
             'mysql_connection': 'US_DW_UNIODS_SSH',
             'batch_size': 50000,
-            'excluded_columns': ['id']
+            'excluded_columns': ['id'],
+            'column_defaults': {'tel': ''}
         },
         'uni_tracking_info': {
             'mysql_table': 'uniods.dw_uni_tracking_info',
@@ -139,6 +140,7 @@ class RawDataBackfiller:
         # Get column names and indices to keep
         raw_column_names = [desc[0] for desc in mysql_cursor.description]
         excluded_cols = table_config.get('excluded_columns', [])
+        column_defaults = table_config.get('column_defaults', {})
         
         # Calculate indices to keep
         keep_indices = [i for i, col in enumerate(raw_column_names) if col not in excluded_cols]
@@ -177,8 +179,17 @@ class RawDataBackfiller:
                 # Format values for SQL safety
                 values_list = []
                 for row in batch:
-                    # Filter row values based on keep_indices
-                    filtered_row = [row[i] for i in keep_indices]
+                    # Filter row values and apply defaults
+                    filtered_row = []
+                    for i in keep_indices:
+                        col_name = raw_column_names[i]
+                        val = row[i]
+                        
+                        # Apply default if value is None and a default exists
+                        if val is None and col_name in column_defaults:
+                            val = column_defaults[col_name]
+                            
+                        filtered_row.append(val)
                     
                     # Use mogrify to safely format values (handles quotes, nulls, dates, etc.)
                     formatted_val = redshift_cursor.mogrify(row_template, filtered_row).decode('utf-8')
